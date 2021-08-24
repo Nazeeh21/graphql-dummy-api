900c6cf3c826147743b8037f18400d74d547a635
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-08-24 11:08:17 +0530

    Add ssl in index.ts

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,9 @@ const main = async () => {
   const conn = await createConnection({
     type: 'postgres',
     url: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     logging: true,
     synchronize: true,
     migrations: [path.join(__dirname, './migrations/*')],
