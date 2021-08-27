c32e26fc2c89e8475037af51ebd2c9c81d6392af
Nazeeh21 <nazeehvahora.786@gmail.com>
2021-08-27 12:09:36 +0530

    remove cursor argument from post resolver and update return method of getPosts query

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,10 +1,11 @@
 import {
-  Arg, Field,
+  Arg,
+  Field,
   FieldResolver,
   InputType,
   Int,
   Mutation,
-  ObjectType,
+  // ObjectType,
   Query,
   Resolver,
   Root,
@@ -21,14 +22,11 @@ class PostInput {
   text: string;
 }
 
-@ObjectType()
-class PaginatedPosts {
-  @Field(() => [Post])
-  posts: Post[];
-
-  @Field()
-  hasMore: boolean;
-}
+// @ObjectType()
+// class PaginatedPosts {
+//   @Field(() => [Post])
+//   posts: Post[];
+// }
 
 @Resolver(Post)
 export class PostResolver {
@@ -37,50 +35,25 @@ export class PostResolver {
     return root.text.slice(0, 50);
   }
 
-  @Query(() => PaginatedPosts)
-  async posts(
-    @Arg('limit', () => Int) limit: number,
-    @Arg('cursor', () => String, { nullable: true }) cursor: string | null
-  ): Promise<PaginatedPosts> {
-    // 20 -> 21
+  @Query(() => [Post])
+  async posts(@Arg('limit', () => Int) limit: number): Promise<Post[]> {
     const realLimit = Math.min(50, limit);
 
     const realLimitPlusOne = realLimit + 1;
 
     const replacements: any[] = [realLimitPlusOne];
 
-    if (cursor) {
-      replacements.push(new Date(+cursor));
-    }
-
     const posts = await getConnection().query(
       `
       select p.*
       from post p
-      ${cursor ? `where p."createdAt" < $2` : ''}
       order by p."createdAt" DESC
       limit $1
     `,
       replacements
     );
-    // return Post.find();
-    // const qb = await getConnection()
-    //   .getRepository(Post)
-    //   .createQueryBuilder('p')
-    //   .innerJoinAndSelect('p.creator', 'u', 'u.id = p."creatorId"')
-    //   .orderBy('p."createdAt"', 'DESC')
-    //   .take(realLimitPlusOne);
 
-    // if (cursor) {
-    //   qb.where('p."createdAt" < :cursor', { cursor: new Date(+cursor) });
-    // }
-
-    // const posts = await qb.getMany();
-    console.log(posts);
-    return {
-      posts: posts.slice(0, realLimit),
-      hasMore: posts.length === realLimitPlusOne,
-    };
+    return posts.slice(0, realLimit);
   }
 
   @Query(() => Post, { nullable: true })
@@ -98,7 +71,7 @@ export class PostResolver {
   async updatePost(
     @Arg('id', () => Int) id: number,
     @Arg('title', { nullable: true }) title: string,
-    @Arg('text', { nullable: true }) text: string,
+    @Arg('text', { nullable: true }) text: string
   ): Promise<Post | null> {
     // return Post.update({id, creatorId: req.session.userId}, {title, text});
 
@@ -118,9 +91,7 @@ export class PostResolver {
   }
 
   @Mutation(() => Boolean)
-  async deletePost(
-    @Arg('id', () => Int) id: number,
-  ): Promise<boolean> {
+  async deletePost(@Arg('id', () => Int) id: number): Promise<boolean> {
     // not cascade way
     // const post = await Post.findOne(id);
 
